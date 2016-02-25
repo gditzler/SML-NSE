6b0635999c56636de501c77caeae5a28f3f50469
Gregory Ditzler <gditzler@Gregorys-Mac-mini.local>
2016-02-24 17:26:13 -0700

    updated datastats

--- src/utils/data_stats.m
+++ src/utils/data_stats.m
@@ -124,10 +124,44 @@ clear;
 close all;
 
 
-datasets = {'a8a', 'german', 'magic04', 'spambase', 'splice', 'svmguide3', ... 
-  'ionosphere', 'ovariancancer', 'arrhythmia', 'sido0',...
-  'miniboone.csv', 'breast-cancer-wisc-diag.csv', 'breast-cancer-wisc-prog.csv', 'chess-krvkp.csv','conn-bench-sonar-mines-rocks.csv',...
-  'connect-4.csv','molec-biol-promoter.csv', 'parkinsons.csv', 'spect_train.csv'};
+datasets = {
+    'adult_train.csv'
+    'bank.csv'
+    'blood.csv'
+    'breast-cancer-wisc-diag.csv'
+    'breast-cancer-wisc-prog.csv'
+    'breast-cancer-wisc.csv'
+    'breast-cancer.csv'
+    'chess-krvkp.csv'
+    'congressional-voting.csv'
+    'conn-bench-sonar-mines-rocks.csv'
+    'connect-4.csv'
+    'credit-approval.csv'
+    'cylinder-bands.csv'
+    'heart-hungarian.csv'
+    'hill-valley_train.csv'
+    'horse-colic_train.csv'
+    'ilpd-indian-liver.csv'
+    'ionosphere.csv'
+    'magic.csv'
+    'mammographic.csv'
+    'miniboone.csv'
+    'mushroom.csv'
+    'musk-2.csv'
+    'oocytes_merluccius_nucleus_4d.csv'
+    'oocytes_trisopterus_nucleus_2f.csv'
+    'ozone.csv'
+    'pima.csv'
+    'ringnorm.csv'
+    'spambase.csv'
+    'statlog-australian-credit.csv'
+    'statlog-german-credit.csv'
+    'statlog-heart.csv'
+    'tic-tac-toe.csv'
+    'titanic.csv'
+    'twonorm.csv'
+    'vertebral-column-2clases.csv'
+  };
 
 
 for dd = 1:length(datasets)
