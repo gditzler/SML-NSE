c1535c0cdee90836a7e90c06662a9d17755ce4b0
Gregory Ditzler <ditzler@email.arizona.edu>
2016-05-19 11:13:37 -0700

    changed ranks]

--- src/utils/rank_rows.m
+++ src/utils/rank_rows.m
@@ -1,8 +1,5 @@
 function R = rank_rows(X)
 R = zeros(size(X));
 for i = 1:size(X, 1)
-  [~, idx] = sort(X(i, :));
-  for j = 1:length(idx)
-    R(i, idx(j)) = j;
-  end
+  R(i, :) = fracrank(X(i, :))';
 end
