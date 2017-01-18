5f6618560b8f42ddb3642608105e4381011fd1b4
Gregory Ditzler <ditzler@email.arizona.edu>
2017-01-18 15:29:34 -0700

    updated for an experiment

--- src/experiment_nonstationary_missing_datastreams.m
+++ src/experiment_nonstationary_missing_datastreams.m
@@ -17,7 +17,7 @@ addpath('data/');
 addpath(genpath('SCARGC_codes/'));
 
 % free parameters of the experiement
-miss_amt = 1;         % percentange of missing training data 
+miss_amt = 2;         % percentange of missing training data 
 end_experiment = 0;   % test-then-train or test-on-last
 avg = 10;             % number of averages to perform  
 alpha = .7;           % exponential forgetting factor for CVX-sense
@@ -48,47 +48,102 @@ for dd = 1:length(datasets)
   % these cases we need to make sure that all of the data from different
   % sources are formated the same when we exit the conditional statements.
   % the data will be stored in alldata and allclass. 
-  if strcmp(datasets{dd}, 'ionosphere')
-    load ionosphere
-    [~,~,y] = unique(Y);
-    allclass = y;
-    alldata(:, 2) = [];
-  elseif strcmp(datasets{dd}, 'ovariancancer')
-    load ovariancancer
-    [~,~,y] = unique(grp);
-    allclass = y;
-    alldata = obs;
-  elseif strcmp(datasets{dd}, 'arrhythmia')
-    load arrhythmia
-    dels = find(Y==16);
-    Y(dels) = [];
-    X(dels, :) = [];
-    X(:, [11,2,14]) = [];
-    z = sum(isnan(X),2);
-    X(z==1, :) = [];
-    Y(z==1) = [];
-    Y(Y~=1) = -1;
-    Y(Y==-1) = 2;
-    data = [Y X];
-    alldata = X;
-    allclass = Y;
-    clear dels Description VarNames X Y z
-  elseif length(findstr('csv', datasets{dd})) > 0
-    data = load(['../../ClassificationDatasets/csv/', datasets{dd}]);
-    X = data(:, 1:end-1);
-    Y = data(:, end);
-    Y(Y == 0) = 2;
-    X = X(:, std(X)~=0);
-    alldata = X;
-    allclass = Y;
+  % set a window size based on the size of the data set. given that we have
+  % very different data set sizes, we need to have a way to make sure we do
+  % not have a data set with 2 time stamps and 2000 time stamps. 
+  if strcmp(dat,'noaa')
+    load neweather_rain;
+    alldata = alldata';   % old format of data needs transpose
+    allclass = allclass'; % ^^^^^
+    win_size = 120;    % size of train / test batch (3 months - 1 season)
+    mclass = 2;
+  elseif strcmp(dat,'air')
+    alldata = load('data/air2.arff');
+    mclass = 2;
+    allclass = alldata(:, 8);
+    allclass = allclass + 1;
+    alldata(:, 8) = [];
+    win_size = 1200;     
+  elseif strcmp(dat,'poker')
+    alldata = load('data/poker.arff');
+    alldata(alldata(:, end) == 9, :) = [];
+    alldata(alldata(:, end) == 8, :) = [];
+    alldata(alldata(:, end) == 7, :) = [];
+    alldata(alldata(:, end) == 6, :) = [];
+    alldata(alldata(:, end) == 5, :) = [];
+    alldata(alldata(:, end) == 4, :) = [];
+    alldata(alldata(:, end) == 3, :) = [];
+    allclass = alldata(:, end);
+    allclass = allclass + 1;
+    allclass(allclass == 3) = 2;
+    alldata(:, end) = [];
+    win_size = 2000; 
+    mclass = 2;
+    % figure; hold on
+    % plot(find(allclass==1), cumsum(allclass(allclass==1)))
+    % plot(find(allclass==2), cumsum(allclass(allclass==2)==2))
+  elseif strcmp(dat,'rbf')
+    alldata = load('data/rbf150k.arff');
+    mclass = 2;
+    allclass = alldata(:, end);
+    allclass = allclass + 1;
+    alldata(:, end) = [];
+    win_size = 1200; 
+  elseif strcmp(dat,'elec2')
+    load elec2;
+    alldata(3,:) = []; % remove the "cheating" features
+    alldata(4,:) = []; 
+    win_size = 125;    % size of train / test batch
+    alldata = alldata';   % old format of data needs transpose
+    allclass = allclass'; % ^^^^^
+    mclass = 2;
+  elseif strcmp(dat,'dataLU')
+    load dataLU
+    alldata = dataLU.X';
+    allclass = dataLU.y' + 1;
+    win_size = 100;
+    mclass = max(allclass);
+  elseif strcmp(dat,'chess')
+    load chessIZ
+    alldata = alldata';   % old format of data needs transpose
+    allclass = allclass'; % ^^^^^
+    alldata(allclass==3, :) = [];
+    allclass(allclass==3) = [];
+    mclass = max(allclass);
+    win_size = 35;
+  elseif strcmp(dat,'spam')
+    load spam2
+    alldata = alldata';   % old format of data needs transpose
+    allclass = allclass'; % ^^^^^
+    mclass = max(allclass);
+    win_size = 100;
+  elseif strcmp(dat, 'sea')
+    win_size = 250;
+    len = 200;
+    noise = .1;
+    alldata = 10 * rand(win_size * len,3);
+    s1 = sum(alldata(1:floor(win_size * len / 4),1:2),2);
+    s1(s1 > 8) = 1;
+    s1(s1 ~= 1) = 2;
+    s2 = sum(alldata(1+floor(win_size * len / 4):2*floor(win_size * len / 4),1:2),2);
+    s2(s2 > 9) = 1;
+    s2(s2 ~= 1) = 2;
+    s3 = sum(alldata(1+2*floor(win_size * len / 4):3*floor(win_size * len / 4),1:2),2);
+    s3(s3 > 7.5) = 1;
+    s3(s3 ~= 1) = 2;
+    s4 = sum(alldata(1+3*floor(win_size * len / 4):end,1:2),2);
+    s4(s4 > 9.5) = 1;
+    s4(s4 ~= 1) = 2;
+    allclass = [s1;s2;s3;s4];
+    mclass = 2;
+    win_size = win_size-1;
   else
-    load(['../../OFSE/data/', datasets{dd}, '.mat'])
-    X = data(:, 2:end);
-    Y = data(:, 1);
-    X = X(:, std(X)~=0);
-    Y(Y==-1) = 2;
-    alldata = X;
-    allclass = Y;
+    data = load(dat);
+    allclass = data(:, end)+1;
+    alldata = data(:, 1:end-1);    
+    win_size = numel(allclass)/2000;
+    mclass = length(unique(allclass));
+    clear data
   end
   
   [nos, nof] = size(alldata);
@@ -97,18 +152,6 @@ for dd = 1:length(datasets)
   alldata = alldata(shuffs, :);
   allclass = allclass(shuffs);
   
-  % set a window size based on the size of the data set. given that we have
-  % very different data set sizes, we need to have a way to make sure we do
-  % not have a data set with 2 time stamps and 2000 time stamps. 
-  if nos < 1000
-    win_size = 50;
-  elseif nos < 10000
-    win_size = 100;
-  elseif nos < 15000
-    win_size = 250;
-  else
-    win_size = 500;
-  end
   
   % several of the models need to know the number of time stamps in
   % advance, so partition the data into a stream to determine the number of
@@ -164,7 +207,7 @@ for dd = 1:length(datasets)
   time_cvx = zeros(length(data_train), avg);
   time_scar = zeros(length(data_train), avg);
   
-  for i = 1:avg
+  parfor i = 1:avg
     disp(['  -Avg ', num2str(i), '/', num2str(avg)]);
     
     % since these are stationary data streams, we can permuate the entire
