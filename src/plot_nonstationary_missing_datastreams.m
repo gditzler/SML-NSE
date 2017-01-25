f8f5f12a44f49c7c3436de1c9b32452eb7ac1511
Gregory Ditzler <gregory.ditzler@gmail.com>
2017-01-24 22:28:51 -0700

    added local stuff

--- src/plot_nonstationary_missing_datastreams.m
+++ src/plot_nonstationary_missing_datastreams.m
@@ -3,6 +3,7 @@ clear
 close all
 
 end_exper = '';  %'_' or '_END'
+toprint = 'error';
 
 datasets = {
   'noaa'
@@ -13,16 +14,50 @@ datasets = {
   %'air'
   };
 vars2load = {'err_avg'};
-
-for i = 1:3
-  j = i+1;
-  for k = 1:length(datasets)
-    load(['../results/missing_nse_', num2str(j), datasets{k}, end_exper,'_err_kappa.mat'], '-regexp', 'err_*');
-    load('../results/missing_nse_2sea_END_err_kappa.mat', '-regexp', 'kappa_*');
-    load('../results/missing_nse_2sea_END_err_kappa.mat', '-regexp', 'time_*');
+types = {'avg', 'ftl', 'nse', 'sml', 'cvx'};
+for k = 1:length(datasets)  
+  str = ' & ';
+  for t = 1:length(types)-1
+    str = [str, types{t}, ' & '];
+  end
+  str = [str, types{end}, ' \\ '];
+  disp(str);
+  
+  str = [datasets{k}, ' & '];
+  
+  for i = 1:3
+    j = i+1;
+    fname = ['../results/missing_nse_', num2str(j), datasets{k}, end_exper,'_err_kappa.mat'];
+    load(fname, '-regexp', 'err_*');
+    load(fname, 'kappa_*');
+    load(fname, '-regexp', 'time_*');
     clear err_mle err_map err_avg err_scar
     clear kappa_mle kappa_map kappa_avg kappa_scar
     clear time_mle time_map time_avg time_scar
+    
+    idx = 2:size(err_avg_cor, 1)-1;
+    errors.avg = nanmean(err_avg_cor(idx, :), 2);
+    errors.cvx = nanmean(err_cvx(idx, :), 2);
+    errors.ftl = nanmean(err_ftl(idx, :), 2);
+    errors.nse = nanmean(err_nse(idx, :), 2);
+    errors.sml = nanmean(err_sml(idx, :), 2);
+    
+    kappas.avg = nanmean(kappa_avg_cor(idx, :), 2);
+    kappas.cvx = nanmean(kappa_cvx(idx, :), 2);
+    kappas.ftl = nanmean(kappa_ftl(idx, :), 2);
+    kappas.nse = nanmean(kappa_nse(idx, :), 2);
+    kappas.sml = nanmean(kappa_sml(idx, :), 2);
+    clear kappa_* err_* time_*
+     
+    for t = 1:length(types)-1
+      if strcmp(toprint, 'error') == 1
+        str = [str, num2str(mean(getfield(errors, types{t}))), ' & '];
+      else
+        str = [str, num2str(mean(getfield(kappas, types{t}))), ' & '];
+      end
+    end
+    str = [str, num2str(mean(getfield(kappas, types{end}))), ' \\ '];
+    disp([str, '\n'])
   end
 end
 
---
+++ src/plot_stationary_datastreams.m
@@ -0,0 +1,43 @@
+clc; 
+clear; 
+close all;
+
+% data must be downloaded from the UAMLDA Gitlab data set repo 
+datasets = {
+    'adult_train.csv'
+    'bank.csv'
+    'blood.csv'
+    'breast-cancer-wisc-diag.csv'
+    'breast-cancer-wisc-prog.csv'
+    'breast-cancer-wisc.csv'
+    'breast-cancer.csv'
+    'chess-krvkp.csv'
+    'congressional-voting.csv'
+    'conn-bench-sonar-mines-rocks.csv'
+    'credit-approval.csv'
+    'cylinder-bands.csv'
+    'heart-hungarian.csv'
+    'hill-valley_train.csv'
+    'horse-colic_train.csv'
+    'ilpd-indian-liver.csv'
+    'ionosphere.csv'
+    'magic.csv'
+    'mammographic.csv'
+    'mushroom.csv'
+    'musk-2.csv'
+    'oocytes_merluccius_nucleus_4d.csv'
+    'oocytes_trisopterus_nucleus_2f.csv'
+    'pima.csv'
+    'ringnorm.csv'
+    'spambase.csv'
+    'statlog-australian-credit.csv'
+    'statlog-german-credit.csv'
+    'statlog-heart.csv'
+    'tic-tac-toe.csv'
+    'titanic.csv'
+    'twonorm.csv'
+    'vertebral-column-2clases.csv'
+  };
+for d = 1:length(datasets)
+  datasets{d} = strrep(datasets{d}, '.csv', '');
+end
