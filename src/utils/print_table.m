aa9aab0db3174093cd336dd6cb2177d1e24b20aa
Gregory Ditzler <ditzler@email.arizona.edu>
2017-03-27 12:55:00 -0700

    init commit

---
+++ src/utils/print_table.m
@@ -0,0 +1,26 @@
+function print_table(alg_names, data_names, scores, ranks)
+
+str = '';
+for a = 1:length(alg_names)
+  str = [str, ' & \bf ', alg_names{a}];
+end
+disp([str, ' \\ ']); 
+
+
+for d = 1:length(data_names)
+  str = data_names{d};
+  
+  for a = 1:length(alg_names)
+    str = [str, ' & ', num2str(scores(d, a)), '(',  num2str(ranks(d, a)), ') '];
+  end
+  str = [str, ' \\ '];
+  disp(str);
+end
+
+mR = mean(ranks);
+str = 'ranks ';
+for a = 1:length(alg_names)
+  str = [str, ' & ', num2str(mR(a))];
+end
+disp([str, ' \\ ']); 
+
